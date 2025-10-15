c6c2bcaf6bc65be6592c7565a319746e0ca1ef69
bananabi <131904987+bananabi@users.noreply.github.com>
2025-10-15 13:04:02 -0400

    This program gets the distance between objects

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created on: Sep 2025
+ * This program gets the distance between objects
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+//setup 
+basic.showIcon(IconNames.Happy)
+
+// find distance to object 
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
